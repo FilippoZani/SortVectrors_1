49a1ef3515e9d58ea453ac4f15727506d8bad9dd
Filippo Zani <FILIPPO.ZANI@local.ittcesena.it>
2020-10-07 11:47:46 +0200

    Second Step: Ordinamento

--- SortVectors_1/Program.cs
+++ SortVectors_1/Program.cs
@@ -6,13 +6,22 @@ namespace SortVectors_1
     {
         static void Main(string[] args)
         {
-            int[] vector = new int[10];
+            int[] vector = new int[10];   
+
             for(int i = 0; i < vector.Length; i++)
             {
                 Random r = new Random();
                 vector[i] = r.Next(0, 3);
+                Ordina(vector);
                 Console.WriteLine(vector[i]);
             }
+
+            
+
+        }
+        static void Ordina(int[] v)
+        {
+            Array.Sort(v);
         }
     }
 }
