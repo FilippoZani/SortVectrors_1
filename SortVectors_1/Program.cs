d9df9c2b867f45b7347421bc8bc18d8d8f3de602
Filippo Zani <FILIPPO.ZANI@local.ittcesena.it>
2020-10-07 11:06:38 +0200

    First Step

---
+++ SortVectors_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SortVectors_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[10];
+            for(int i = 0; i < vector.Length; i++)
+            {
+                Random r = new Random();
+                vector[i] = r.Next(0, 3);
+                Console.WriteLine(vector[i]);
+            }
+        }
+    }
+}
